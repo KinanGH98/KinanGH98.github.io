0717de5154dd1788543e29fc129813a8696df256
Kinan GH <KinanGH98@gmail.com>
2024-06-02 23:10:15 +0300

    1. The dashboard now supports toggling between dark and light themes. 2. Replaced dashboard cards use of svg file with an embedded version for more styling control. 3. Some dashboard layout bug fixes.

---
+++ index-script.js
@@ -0,0 +1,50 @@
+﻿const hamburgerMenu = document.querySelector(".header-links");
+let menuOpened;
+let currentThemeSetting = localStorage.getItem("theme") ?? "dark";
+setTheme(currentThemeSetting);
+
+window.onresize = menuResized;
+
+function toggleHamburgerMenu()
+{
+    if (menuOpened)
+    {
+        hamburgerMenu.style.display = "none";
+        menuOpened = false;
+    }
+    else if (!menuOpened)
+    {
+        hamburgerMenu.style.display = "flex";
+        menuOpened = true;
+    }
+}
+
+function menuResized()
+{
+    // Enable the links div on desktop. 
+    if (window.innerWidth > 800 && !menuOpened)
+    {
+        toggleHamburgerMenu();
+        return;
+    }
+
+    // Disable the links div on mobile. 
+    if (window.innerWidth < 800 && menuOpened) toggleHamburgerMenu();
+}
+
+function toggleTheme()
+{
+    currentThemeSetting = currentThemeSetting === "dark" ? "light" : "dark";
+    setTheme(currentThemeSetting);
+}
+
+function setTheme(theme)
+{
+    document.querySelector("html").setAttribute("data-theme", theme);
+
+    // Change the toggle button icon.
+    let icon = theme === "light" ? "fa-solid fa-moon" : "fa-solid fa-sun";
+    document.getElementById("themeToggle").querySelector("i").className = icon;
+
+    localStorage.setItem("theme", theme);
+}
---
+++ purple-signup-script.js
@@ -0,0 +1,34 @@
+﻿let passwordConfirmation = document.getElementById("passwordConfirm");
+let originalPassword = document.getElementById("password");
+let dateOfBirth = document.getElementById("birthdate");
+
+passwordConfirmation.addEventListener("input", () =>
+    {
+        if (passwordConfirmation.value === originalPassword.value)
+            passwordConfirmation.setCustomValidity("");
+        else
+            passwordConfirmation.setCustomValidity("Password confirmation doesn't match your password.");
+    }
+);
+
+dateOfBirth.addEventListener("input", () =>
+    {
+        if (getAge(dateOfBirth.value) >= 18)
+            dateOfBirth.setCustomValidity("")
+        else
+            dateOfBirth.setCustomValidity("You must be at least 18 years old.")
+    }
+);
+
+function getAge(DOB)
+{
+    const today = new Date();
+    const birthDate = new Date(DOB);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
+    {
+        age--;
+    }
+    return age;
+}
