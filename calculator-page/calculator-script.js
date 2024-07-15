698a22c8d931341ea8babd17adc30b3655635185
Kinan GH <KinanGH98@gmail.com>
2024-07-15 13:58:44 +0300

    1. Added a lot of projects made by React. 2. Added a Calculator page made using HTML/CSS/JS. 3. Some bug fixes. * This push is a test push for testing the Chaty app.

---
+++ calculator-page/calculator-script.js
@@ -0,0 +1,167 @@
+﻿class Calculator
+{
+    constructor(previousOperandText, currentOperandText)
+    {
+        this.previousOperandText = previousOperandText;
+        this.currentOperandText = currentOperandText;
+        this.clear();
+    }
+
+    clear()
+    {
+        this.previousOperand = '';
+        this.currentOperand = '0';
+        this.operation = undefined;
+    }
+
+    delete()
+    {
+        this.currentOperand = this.currentOperand.slice(0, -1);
+        if (this.currentOperand === '') this.currentOperand = '0';
+    }
+
+    appendNumber(number)
+    {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        if (this.currentOperand === '0' && number !== '.') this.currentOperand = '';
+        this.currentOperand += number.toString();
+    }
+
+    chooseOperation(operation)
+    {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') this.compute();
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '0';
+    }
+
+    compute()
+    {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation)
+        {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '×':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+
+        this.currentOperand = computation.toString();
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+    updateDisplay()
+    {
+        this.currentOperandText.innerHTML = this.currentOperand;
+        this.previousOperandText.innerHTML = this.previousOperand;
+
+        if (this.operation != null)
+            this.previousOperandText.innerText = `${this.previousOperand} ${this.operation}`;
+    }
+}
+
+const numberBtns = Array.from(document.querySelectorAll("[data-number]"));
+const operationBtns = Array.from(document.querySelectorAll("[data-operation]"));
+const equalsBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const clearBtn = document.querySelector("[data-clear]");
+const previousOperandTxt = document.querySelector("[data-previous-operand]");
+const currentOperandTxt = document.querySelector("[data-current-operand]");
+
+let calculator = new Calculator(previousOperandTxt, currentOperandTxt);
+
+numberBtns.forEach(button =>
+    button.addEventListener('click', () =>
+    {
+
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    }));
+
+operationBtns.forEach(button => button.addEventListener('click', () =>
+{
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+}));
+
+equalsBtn.addEventListener('click', () =>
+{
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+clearBtn.addEventListener('click', () =>
+{
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteBtn.addEventListener('click', () =>
+{
+    calculator.delete();
+    calculator.updateDisplay();
+});
+
+// Keyboard shortcuts.
+document.addEventListener('keydown', (e) =>
+{
+    switch (e.key)
+    {
+        case 'Enter':
+        case'=':
+            equalsBtn.click();
+            e.preventDefault();
+            break;
+        case 'Backspace':
+            deleteBtn.click();
+            break;
+        case 'Delete':
+            clearBtn.click();
+            break;
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+        case '.':
+            for (let i = 0; i < numberBtns.length; i++)
+            {
+                if (numberBtns[i].innerText === e.key)
+                {
+                    numberBtns[i].click();
+                    break;
+                }
+            }
+            break;
+        case '+':
+        case '-':
+            operationBtns.find(b => b.innerText === e.key).click();
+            break;
+        case '*':
+            operationBtns.find(b => b.innerText === '×').click();
+            break;
+        case '/':
+            operationBtns.find(b => b.innerText === '÷').click();
+            break;
+    }
+});
--- index-script.js
+++ index-script.js
@@ -1,35 +1,54 @@
 ﻿const hamburgerMenu = document.querySelector(".header-links");
 let menuOpened;
-let currentThemeSetting = localStorage.getItem("theme") ?? "dark";
+let currentThemeSetting = localStorage.getItem("theme") ?? "light";
 setTheme(currentThemeSetting);
 
-window.onresize = menuResized;
+window.onresize = screenResized;
+document.getElementById("hamburgerBtn").addEventListener("click", toggleHamburgerMenu);
 
-function toggleHamburgerMenu()
+function toggleHamburgerMenu(e)
 {
     if (menuOpened)
     {
         hamburgerMenu.style.display = "none";
         menuOpened = false;
+        document.getElementById("hamburgerBtn").querySelector("i").className = "fa-solid fa-bars fa-flip";
+        hamburgerMenu.classList.remove('header-links-fade');
+        document.removeEventListener("click", hideHamburgerMenuOnOutsideClick);
     }
     else if (!menuOpened)
     {
         hamburgerMenu.style.display = "flex";
         menuOpened = true;
+        document.getElementById("hamburgerBtn").querySelector("i").className = "fa-solid fa-xmark fa-spin fa-spin-reverse";
+        document.addEventListener("click", hideHamburgerMenuOnOutsideClick);
     }
+
+    // Prevent the document.onclick event from triggering by disabling event bubbling. 
+    if (e !== undefined) e.stopPropagation();
 }
 
-function menuResized()
+/**
+ * Hide the hamburger menu when the user clicks outside of it.
+ */
+function hideHamburgerMenuOnOutsideClick(mouseClick)
+{
+    if (!document.querySelector(".header-links").contains(mouseClick.target))
+        toggleHamburgerMenu();
+}
+
+/**
+ * Show/Hide the hamburger menu when the screen is resized to certain thresholds.
+ */
+function screenResized()
 {
     // Enable the links div on desktop. 
-    if (window.innerWidth > 800 && !menuOpened)
-    {
-        toggleHamburgerMenu();
-        return;
-    }
-
+    if (window.innerWidth > 800 && !menuOpened) toggleHamburgerMenu();
     // Disable the links div on mobile. 
-    if (window.innerWidth < 800 && menuOpened) toggleHamburgerMenu();
+    else if (window.innerWidth < 800 && menuOpened) toggleHamburgerMenu();
+
+    // Disable hiding the hamburger menu when the screen resizes.
+    document.removeEventListener("click", hideHamburgerMenuOnOutsideClick);
 }
 
 function toggleTheme()
@@ -43,7 +62,7 @@ function setTheme(theme)
     document.querySelector("html").setAttribute("data-theme", theme);
 
     // Change the toggle button icon.
-    let icon = theme === "light" ? "fa-solid fa-moon" : "fa-solid fa-sun";
+    let icon = theme === "light" ? "fa-solid fa-moon fa-fade" : "fa-solid fa-sun fa-beat";
     document.getElementById("themeToggle").querySelector("i").className = icon;
 
     localStorage.setItem("theme", theme);
