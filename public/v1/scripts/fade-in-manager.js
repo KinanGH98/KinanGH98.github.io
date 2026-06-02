5500d448fe9b82b8df701bf337d47369e59f1a5c
Kinan GH <kinangh98@gmail.com>
2026-06-02 15:03:34 +0300

    feat: add version 1 of the portfolio under /public/v1 to keep it accessible as a legacy website.

---
+++ public/v1/scripts/fade-in-manager.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener('DOMContentLoaded', () =>
+{
+    const fadeIns = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries) =>
+    {
+        entries.forEach(entry =>
+        {
+            if (entry.isIntersecting)
+            {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Stop observing once the animation has been triggered
+            }
+        });
+    }, {threshold: 0.5}); // Adjust the threshold as needed
+
+    fadeIns.forEach(fadeIn =>
+    {
+        observer.observe(fadeIn);
+    });
+});
